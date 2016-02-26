e037e677e4d31f8d1597576e5a9dfe94a2e9a3fc
Andreas Alfarè <andreas.alfare@gmail.com>
2016-02-26 16:47:09 +0100

    Initial Commit

---
+++ Formulario/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Formulario
+//
+//  Created by Andreas Alfarè on 26.02.16.
+//  Copyright © 2016 alfare.it. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
