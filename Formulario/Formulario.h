9ba2dee4bc8fbf06623559443674541e58b82cd7
Andreas Alfarè <andreas.alfare@gmail.com>
2016-02-29 16:35:40 +0100

    Adds framework for carthage compatibility

---
+++ Formulario/Formulario.h
@@ -0,0 +1,19 @@
+//
+//  Formulario.h
+//  Formulario
+//
+//  Created by Andreas Alfarè on 29.02.16.
+//  Copyright © 2016 alfare.it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Formulario.
+FOUNDATION_EXPORT double FormularioVersionNumber;
+
+//! Project version string for Formulario.
+FOUNDATION_EXPORT const unsigned char FormularioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Formulario/PublicHeader.h>
+
+
