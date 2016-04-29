f52209d84651121ef01a46d925fd3f8e9f07c5cd
Andreas Alfarè <andreas.alfare@gmail.com>
2016-04-29 17:57:15 +0200

    Adds RemoteForm and RemoteFormViewController

---
+++ Formulario/RemoteFormulario.swift
@@ -0,0 +1,115 @@
+//
+//  RemoteFormulario.swift
+//  Formulario
+//
+//  Created by Andreas Alfarè on 29.04.16.
+//  Copyright © 2016 alfare.it. All rights reserved.
+//
+
+import UIKit
+
+public class RemoteForm: Form {
+    var formUrl: String
+    var title: String?
+    var responseUrl: String?
+    var action: String?
+    var parameters: [String: AnyObject]?
+    
+    public init(formUrl: String) {
+        self.formUrl = formUrl
+        super.init(sections: [])
+    }
+    
+    func loadForm() {
+        guard let url = NSURL(string: formUrl) else {
+            return
+        }
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        session.dataTaskWithURL(url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                guard let json = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: AnyObject] else {
+                    return
+                }
+                self.title = json["title"] as? String
+                self.responseUrl = json["responseUrl"] as? String
+                self.action = json["action"] as? String
+                self.parameters = Dictionary()
+                
+                guard let jsonSections = json["sections"] as? Array<[String: AnyObject]> else {
+                    return
+                }
+                var sections = [FormSection]()
+                for section in jsonSections {
+                    var rows = [FormRow]()
+                    if let jsonRows = section["rows"] as? Array<[String: AnyObject]> {
+                        for row in jsonRows {
+                            rows.append(TextFieldFormRow(title: row["label"] as? String, value: row["value"] as? String, placeholder: row["placeholder"] as? String, cellSelection: nil, valueChanged: { r in
+                                if let name = row["name"] as? String {
+                                    self.parameters?[name] = r.value as? AnyObject
+                                }
+                            }))
+                        }
+                    }
+                    sections.append(Formulario.FormSection(title: section["label"] as? String, rows: rows))
+                }
+                self.sections = sections
+                
+                dispatch_async(dispatch_get_main_queue(), { 
+                    self.tableView?.reloadData()
+                })
+                
+            } catch {
+                
+            }
+        }.resume()
+    }
+    
+    func submit() {
+        guard let responseUrl = responseUrl else {
+            return
+        }
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: responseUrl)!)
+        request.HTTPMethod = action ?? "GET"
+        if let parameters = parameters {
+            print(parameters)
+            
+            
+            var paramString = ""
+            for (key, value) in parameters {
+                let escapedKey = key.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())
+                let escapedValue = value.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())
+                paramString += "\(escapedKey)=\(escapedValue)&"
+            }
+            
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        }
+        
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        session.dataTaskWithRequest(request) { (data, response, error) in
+            
+        }.resume()
+    }
+}
+
+class RemoteFormViewController: FormViewController {
+    var remoteForm: RemoteForm? {
+        get {
+            return form as? RemoteForm
+        }
+        set {
+            if let newValue = newValue{
+                form = newValue
+            }
+        }
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        remoteForm?.loadForm()
+    }
+}
